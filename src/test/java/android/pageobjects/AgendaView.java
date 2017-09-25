4ecad38d5a332f80c2639ea1f9e37c77dd2b29fb
sirdir <phddir@gmail.com>
2017-09-26 00:19:47 +0300

    some changes

--- src/test/java/android/pageobjects/AgendaView.java
+++ src/test/java/android/pageobjects/AgendaView.java
@@ -6,6 +6,7 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AndroidFindBys;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
@@ -19,28 +20,40 @@ public class AgendaView extends AbstractView {
 
     String partialtext = "MY SCHEDULE";
 
-    @AndroidFindBy(className = "com.pgssoft.testwarez:id/llEventListItem")
-    public List<AndroidElement> agendaList;
+//    @AndroidFindBy(className = "com.pgssoft.testwarez:id/llEventListItem")
+//    public List<AndroidElement> agendaList;
 
-    @AndroidFindBy(id = "com.pgssoft.testwarez:id/favorite")
-    public List<AndroidElement> favoritesCalendarButton;
+    public By agendaList = By.className("com.pgssoft.testwarez:id/llEventListItem");
+
+//    public String agendaList = "com.pgssoft.testwarez:id/llEventListItem";
+
+
+//    @AndroidFindBy(id = "com.pgssoft.testwarez:id/favorite")
+//    public List<AndroidElement> favoritesCalendarButton;
+
+    public By favoritesCalendarButton = By.id("com.pgssoft.testwarez:id/favorite");
+
+//    public String favoritesCalendarButton = "com.pgssoft.testwarez:id/favorite";
 
     public void checkIfAgendaViewIsVisible() {
-        wait.until(ExpectedConditions.visibilityOf(agendaList.get(0)));
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.className(agendaList), 0));
     }
 
     public void addFirstEventToFavorites() {
-        favoritesCalendarButton.get(0).click();
-        wait.until(ExpectedConditions.visibilityOf(agendaList.get(0)));
+        List<AndroidElement> list1 = driver.findElements(favoritesCalendarButton);
+        list1.get(0).click();
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(agendaList, 0));
     }
 
     public void selectSchedulesList() {
-        wait.until(ExpectedConditions.visibilityOf(agendaList.get(0)));
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(agendaList, 0));
+
     }
 
     public void checkIfEventExistInSchedule() {
         driver.findElementByAndroidUIAutomator("new UiSelector().textContains(\""+partialtext+"\")").click();
-        Assert.assertTrue(favoritesCalendarButton.get(0).isEnabled());
+        List<AndroidElement> list1 = driver.findElements(favoritesCalendarButton);
+        Assert.assertTrue(list1.get(0).isEnabled());
     }
 
 //    public boolean (AndroidElement agendaList, int timeout){
